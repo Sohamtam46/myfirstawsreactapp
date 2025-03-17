da16ff25119b884f87430fcb363603ff4047726f
Sohamtam46 <sohamtam46@gmail.com>
2025-03-17 19:33:20 +0000

    added delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,20 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deletetodo(id : string) {
+    client.models.Todo.delete({id})
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+          onClick={() => deletetodo(todo.id)}
+          key={todo.id}>{todo.content}
+          </li>
         ))}
       </ul>
       <div>
